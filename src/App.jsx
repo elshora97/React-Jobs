695d12da3c9108a6b2b61bbe07e06a6058ed9f4d
elshora97 <A.elshora@dataserve.com.sa>
2024-05-16 02:18:34 +0300

    added navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import Navbar from "./components/Navbar";
 
 const App = () => {
-  return <div className="text-5xl">App App</div>;
+  return (
+    <>
+      <Navbar />
+    </>
+  );
 };
 
 export default App;
